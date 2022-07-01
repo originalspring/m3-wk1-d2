a520733db944af6c8cedfd5c80981db84445795d
originalspring <105029639+originalspring@users.noreply.github.com>
2022-06-30 20:23:09 -0700

    homework

---
+++ myapp/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port =3000;
+
+app.get('/', (req,res)=>{
+res.send("hello world gthis is express")});
+
+app.listen(port,() =>{
+console.log('example on $(port)!')
+
+
+
+});
+
